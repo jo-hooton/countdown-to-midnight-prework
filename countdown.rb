0f81bab3191773d6a911e6f4d4219a8676036826
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-10 19:44:46 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(number)
     end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  number = 10
+    while number > 0
+      puts "#{number} SECONDS(S)!"
+      sleep(1)
+      number -= 1
+    end
+  return "HAPPY NEW YEAR!"
+end
